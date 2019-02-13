82cac050daba6ad190ade13acb55f1a4299df021
Kurtis Catlett <>
2019-02-13 16:47:30 -0500

    Added an Array and keys.  I think.

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+
+
+var repositoryArray = ['Bulbasaur', 'Ivysaur', 'Venusaur'];
+
+var Bulbasaur = {
+  Name: Bulbasaur,
+  Height: 0.7,
+  Types: 'Grass', 'Poison'
+};
+
+var Ivysaur = {
+  Name: Ivysaur,
+  Height: 1,
+  Types: 'Grass', 'Poison'
+}
+
+var Venusaur = {
+  Name: Venusaur,
+  Height: 2,
+  Types: 'Grass', 'Poison'
+}
+
+console.log(name);
