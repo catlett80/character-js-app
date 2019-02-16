0fb7744b62cd9752aa892e9e5821c957887920cd
Kurtis Catlett <>
2019-02-16 09:22:00 -0500

    made fixes and added for loop to display key information

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,36 @@
 
 
-var repositoryArray = ['Bulbasaur', 'Ivysaur', 'Venusaur'];
+var repository = [];
+
 
 var Bulbasaur = {
-  Name: Bulbasaur,
-  Height: 0.7,
-  Types: 'Grass', 'Poison'
+  name: 'Bulbasaur',
+  height: '0.7',
+  types: ['Grass', 'Poison']
 };
 
 var Ivysaur = {
-  Name: Ivysaur,
-  Height: 1,
-  Types: 'Grass', 'Poison'
-}
+  name: 'Ivysaur',
+  height: '1',
+  types: ['Grass', 'Poison']
+};
 
 var Venusaur = {
-  Name: Venusaur,
-  Height: 2,
-  Types: 'Grass', 'Poison'
+  name: 'Venusaur',
+  height: '2',
+  types: ['Grass', 'Poison']
+};
+
+repository.push(Bulbasaur);
+repository.push(Ivysaur);
+repository.push(Venusaur);
+console.log(repository)
+
+
+for (var i = 0; i < repository.length; i++){
+  var message =""
+  if(repository[i].height>1){
+    message= "Wow that's big!"
+  }
+  document.write(repository[i].name + ":" + repository[i].height + " " + message + "<br>");
 }
