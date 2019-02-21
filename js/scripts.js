64c6a300aa5c728286a2d7e3c46994c6d0429da2
Kurtis Catlett <>
2019-02-21 18:05:40 -0500

    Added functions and other parts of the assignment

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,18 @@
 
 
   var pokemonRepository = (function pokemon() {
-    var data = {
+
+    var repository = [
+      {
       name: 'Bulbasaur',
       height: '0.7',
       types: ['Grass', 'Poison']
-
-
     }
-  }
+];
 
-
-var Bulbasaur = {
-
+function add(repository){
+  repository.push(Ivysaur);
+  repository.push(Vensaur);
 };
 
 var Ivysaur = {
@@ -26,20 +26,24 @@ var Venusaur = {
   name: 'Venusaur',
   height: '2',
   types: ['Grass', 'Poison']
-  };
 };
 
-
-repository.push(Bulbasaur);
-repository.push(Ivysaur);
-repository.push(Venusaur);
 console.log(repository)
 
+function getAll(){
+  return repository;
+};
 
-for (var i = 0; i < repository.length; i++){
+return {
+  add: add,
+  getAll: getAll
+};
+
+})();
+
+
+repository.forEach(var i = 0; i < repository.length; i++){
   var message =""
   if(repository[i].height>1){
     message= "Wow that's big!"
   }
-  document.write(repository[i].name + ":" + repository[i].height + " " + message + "<br>");
-}
